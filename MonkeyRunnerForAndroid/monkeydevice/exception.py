56051062184e88df5b9d19ceebd08bb0ef2e37ea
Jzaca-RC-Wu <545653435@qq.com>
2018-02-09 18:45:19 +0800

    Update exception.py

--- MonkeyRunnerForAndroid/monkeydevice/exception.py
+++ MonkeyRunnerForAndroid/monkeydevice/exception.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''
-Created on 2015年1月23日
-
-@author: xuxu
-'''
 class SriptException(Exception):
     def __init__(self, str):
         self.str = str
